33b627e900d77a4052d7d077bdbf2c998106ce50
hamza7saeed <126345476+hamza7saeed@users.noreply.github.com>
2023-11-21 15:59:28 +0000

    Add files via upload

---
+++ Class 5/index.js
@@ -0,0 +1,101 @@
+/*Assignments
+
+*Simple Mark Sheet Assignment in JavaScript*
+
+*Objective:* Create a basic program using hardcoded values to determine a student's grade.
+
+*Task:*
+Write a JavaScript program that uses hardcoded marks in three subjects (Math, English, and Science) and assigns a grade based on the average percentage.
+
+*Steps:*
+1. Hardcode values for Math, English, and Science marks.
+2. Calculate the average percentage using the formula: `(Math + English + Science) / 3`.
+3. Implement a grading system using logical operators:
+   - If the average is greater than or equal to 90%, assign an 'A' grade.
+   - If the average is between 80% and 89%, assign a 'B' grade.
+   - If the average is between 70% and 79%, assign a 'C' grade.
+   - If the average is between 60% and 69%, assign a 'D' grade.
+   - If the average is below 60%, assign an 'F' grade.
+4. Display the calculated percentage and assigned grade.*/
+var math = 99;
+var english = 95;
+var science = 79;
+var per = (math + english + science) / 3;
+console.log(per, "%");
+if (per >= 90) {
+    console.log("Student got A grade");
+}
+else if (per > 80 && per < 89) {
+    console.log("Student got B grade");
+}
+else if (per > 70 && per < 79) {
+    console.log("Student get C grade");
+}
+else if (per >= 60 && per < 69) {
+    console.log("Student get D grade");
+}
+else {
+    console.log("Student get F grade");
+}
+/*Compound Expressions Assignment in JavaScript*
+
+*Objective:* Develop a program that utilizes compound expressions to make
+decisions based on multiple conditions.
+
+*Task:*
+Write a JavaScript program that takes hardcoded values for a
+student's marks in two subjects (Math and English) and determines eligibility for an
+ advanced placement course.
+
+*Steps:*
+1. Hardcode values for Math and English marks.
+2. Implement a compound expression using logical operators:
+   - To be eligible for the advanced placement course, the student must have:
+     - A Math mark greater than or equal to 80%.
+     - An English mark greater than or equal to 75%.
+3. Display a message indicating whether the student is eligible for the advanced placement course.
+
+This assignment focuses on the use of compound expressions to make decisions based on multiple
+ conditions, reinforcing the understanding of logical operators and their combination
+ in programming.*/
+var Mmath = 80;
+var English = 80;
+if (Mmath >= 80 && English >= 75) {
+    console.log("Student is eligible for the advanced placement course");
+}
+else {
+    console.log("Student is not eligible for the advanced placement course");
+}
+/*Nested IF Assignment in JavaScript*
+
+*Objective:* Create a program using nested if statements to determine a person's eligibility to vote based on age and nationality.
+
+*Task:*
+Write a JavaScript program that takes hardcoded values for a person's age and nationality and determines whether they are eligible to vote.
+
+*Steps:*
+1. Hardcode values for the person's age and nationality.
+2. Use a nested if statement to check the age:
+  - If the person is 18 years or older, proceed to the next step.
+  - If the person is younger than 18, display a message indicating they are not eligible to vote.
+3. Within the nested block for age, use another if statement to check nationality:
+  - If the person is of a specific nationality (you can choose), display a message indicating they are eligible to vote.
+  - If the person is not of the specified nationality, display a message indicating they are not eligible to vote.
+
+This assignment emphasizes the use of nested if
+statements to handle multiple conditions and decisions in
+a structured manner.
+It also introduces the concept of eligibility for voting based on age and nationality.*/
+var userAge = 18;
+var userNationality = "Pakistan";
+if (userAge >= 18) {
+    if (userNationality == "Pakistani") {
+        console.log("Your are eligible for vote");
+    }
+    else {
+        console.log("You are international immigrant");
+    }
+}
+else {
+    console.log("You are not adult");
+}
